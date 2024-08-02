b6d23b955fdb50613a0c1ee6509327c83b02341f
Gerick Toro <tu_correo_electronico@example.com>
2024-08-02 15:02:18 -0400

    fixed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="text-clean",  # Reemplaza con el nombre de tu librería
+    name="text_clean",  # Reemplaza con el nombre de tu librería
     version="0.1.0",  # Inicia con una versión y actualízala con cada cambio
     author="Gerick Toro",
     author_email="gerickt@gmail.com",
