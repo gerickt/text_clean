d1c4c30fb84d54fdc2f5fbde72e70db35b418fb7
Gerick Toro <tu_correo_electronico@example.com>
2024-08-02 15:56:35 -0400

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="text_clean",
-    version="0.1.0",
+    version="0.1.1",
     author="Gerick Toro",
     author_email="gerickt@gmail.com",
     description="Librería para limpieza de texto en español.",
